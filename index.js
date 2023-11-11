0301437702d6d61b3a7eb2f20c1e4486a91bdbb2
Gaurav Pratap Singh <94060174+Gps-Gaurav@users.noreply.github.com>
2023-11-11 12:28:43 +0530

    commit

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 var cors = require('cors');
 const connection = require('./connection');
 const userRoute = require('./routes/user');
+const categoryRoute = require('./routes/category');
+const productRoute = require('./routes/product');
+
 const app = express();
 
 
@@ -9,6 +12,8 @@ app.use(cors());
 app.use(express.urlencoded({ extended:true}));
 app.use(express.json());
 app.use('/user',userRoute);
+app.use('/category',categoryRoute);
+app.use('/product',productRoute);
 
 module.exports = app;
 
