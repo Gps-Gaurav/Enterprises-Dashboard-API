2f27cf812c617c14f4a7680e46927f83dc2290df
Gaurav Pratap Singh <94060174+Gps-Gaurav@users.noreply.github.com>
2023-11-12 17:50:21 +0530

    commit

--- routes/bill.js
+++ routes/bill.js
@@ -14,20 +14,20 @@ router.post('/generateReport', auth.authenticateToken, (req, res, next) => {
     var productdetailsReport = JSON.parse(orderDetails.productDetails);
 
     var query = "insert into bill (name,uuid,email,contactNumber,paymentMethod,total, productDetails,createdBy) values (?,?,?,?,?,?,?,? )";
-    connection.query(query, [orderDetails.name, generatedUuid, orderDetails.email, orderDetails.contactNumber, orderDetails.paymentMethod, orderDetails.total, orderDetails.productDetails, res.locals.email], (err, results) => {
+    connection.query(query, [orderDetails.name, generatedUuid, orderDetails.email, orderDetails.contactNumber, orderDetails.paymentMethod, orderDetails.totalAmount, orderDetails.productDetails, res.locals.email], (err, results) => {
 
         if (!err) {
             ejs.renderFile(path.join(__dirname, '', "report.ejs"), {
                 productDetails: productdetailsReport,
                 name: orderDetails.name, email: orderDetails.email, contactNumber: orderDetails.contactNumber,
-                paymentMethod: orderDetails.paymentMethod, total: orderDetails.total
+                paymentMethod: orderDetails.paymentMethod, totalAmount: orderDetails.totalAmount
             }
                 , (err, results) => {
                     if (err) {
                         return res.status(500).json(err);
                     }
                     else {
-                        pdf.create(results).toFile('./generated_pdf/' + generatedUuid + ".pdf", function (err, results) {
+                        pdf.create(results).toFile('./generated_pdf/' + orderDetails.uuid + ".pdf", function (err, data) {
                             if (err) {
                                 console.log(err);
                                 return res.status(500).json(err);
@@ -40,10 +40,79 @@ router.post('/generateReport', auth.authenticateToken, (req, res, next) => {
                         })
                     }
                 })
-        } else {
+        }
+        else {
             return res.status(500).json(err);
         }
     });
 })
 
+router.post('/getPdf', auth.authenticateToken, function (req, res) {
+    const orderDetails = req.body;
+    const pdfPath = './generated_pdf/' + orderDetails.uuid + '.pdf';
+    if (fs.existsSync(pdfPath)) {
+        res.contentType('application/pdf');
+        fs.createReadStream(pdfPath).pipe(res);
+    }
+    else {
+        var productdetailsReport = JSON.parse(orderDetails.productDetails);
+        ejs.renderFile(path.join(__dirname, '', "report.ejs"), {
+            productDetails: productdetailsReport,
+            name: orderDetails.name, email: orderDetails.email, contactNumber: orderDetails.contactNumber,
+            paymentMethod: orderDetails.paymentMethod, totalAmount: orderDetails.totalAmount
+        }
+            , (err, results) => {
+                if (err) {
+                    return res.status(500).json(err);
+                }
+                else {
+                    pdf.create(results).toFile('./generated_pdf/' + orderDetails.uuid + ".pdf", function (err, data) {
+                        if (err) {
+                            console.log(err);
+                            return res.status(500).json(err);
+                        }
+                        else {
+                            res.contentType('application/pdf');
+                            fs.createReadStream(pdfPath).pipe(res);
+                        }
+
+
+                    })
+                }
+            })
+
+    }
+
+})
+
+router.get('/getBills', auth.authenticateToken, (req, res) => {
+    var query = "select * from bill order by id DESC";
+    connection.query(query, (err, results) => {
+        if (!err) {
+            return res.status(200).json(results);
+        }
+        else {
+            return res.status(500).json(err);
+        }
+    })
+})
+
+
+router.delete('/delete/:id', auth.authenticateToken, (req, res) => {
+    const id = req.params.id;
+    var query = "delete from bill where id =?";
+    connection.query(query, [id], (err, results) => {
+        if (!err) {
+            if (results.affectedRows == 0) {
+                return res.status(404).json({
+                    message: "Bill not found"
+                })
+            }
+            return res.status(200).json({ message: "Bill deleted successfully" })
+        }
+        else {
+            return res.status(500).json(err);
+        }
+    })
+})
 module.exports = router;
